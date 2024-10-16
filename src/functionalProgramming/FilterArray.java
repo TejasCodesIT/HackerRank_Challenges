7c4b330a24a3ad6f20f638628dc02fb494acc6f3
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-10-16 20:11:30 +0530

    Created project

---
+++ src/functionalProgramming/FilterArray.java
@@ -0,0 +1,20 @@
+package functionalProgramming;
+
+import java.util.Scanner;
+
+public class FilterArray {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a value which should is ");
+		int value=  sc.nextInt();
+		
+		
+		
+		
+	}
+
+}
---
+++ src/functionalProgramming/InputOutput.java
@@ -0,0 +1,22 @@
+package functionalProgramming;
+
+import java.util.Scanner;
+
+public class InputOutput {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Please Enter first Number :");
+		int a = sc.nextInt();
+		
+		System.out.println("Please Enter secound Number : ");
+	   int b= 	sc.nextInt();
+		
+		System.out.println("Outpur :"+(a+b));
+		
+	}
+
+}
