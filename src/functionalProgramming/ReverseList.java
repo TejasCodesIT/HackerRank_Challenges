5caa3e449469b7bdbbb31409dd05caf641ff5e8d
Wakchaure123 <133526230+Wakchaure123@users.noreply.github.com>
2024-10-16 21:54:57 +0530

    Reverse an Array

---
+++ src/functionalProgramming/ReverseList.java
@@ -0,0 +1,17 @@
+package functionalProgramming;
+
+public class ReverseList {
+	
+	public static void main(String[] args) {
+		
+		
+		int[] array = {19,22,3,28,26,17,18,4,28,0};
+		
+		for(int i = array.length-1;i>=0;i--) {
+			System.out.println(array[i]);
+		}
+		
+		
+	}
+
+}
